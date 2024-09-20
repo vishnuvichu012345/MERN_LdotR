d6e0178b0b1e3c9e35b40bb1843d3039a70e6166
vishnuvichu012345 <vuvishnu23@gmail.com>
2024-09-20 16:02:48 +0530

    Qs4_QS5

--- server/server.js
+++ server/server.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/User');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+
+
+
+mongoose.connect('mongodb+srv://vuvishnu23:vuvishnu23@cluster0.rhz5j.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const user = new User({ name: 'Alice', email: 'alice@example.com', age: 28 });
+// user.save().then(() => console.log('User added'));
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+
+
+app.get('/users/:email', (req, res) => {
+  User.findOne({ email: req.params.email })
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Error fetching user', details: err });
+    });
+});
+
+app.get('/users/:email', async (req, res) => {
+  const user = await User.findOne({ email: req.params.email });
+  res.json(user);
+  console.log(json(user))
 });
 
 
-app.get('/users',(req,res)=>{
-  const users=[
-    {
-      name:'vishnu',email:'vuvishnu23@gmail.com',age:30
-    },
-    {
-      name:'jishnu',email:'vujishnu23@gmail.com',age:23
-    },
-   
-  ]
-  res.json(users)
-})
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
